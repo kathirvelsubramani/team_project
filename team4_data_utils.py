3979cf30483888b3c5d5bc94e400437f2724c5e7
Kathirvel <kathirsubaramani44@gmail.com>
2025-11-06 13:59:10 +0530

    I added a LCM function

--- team4_data_utils.py
+++ team4_data_utils.py
@@ -1,3 +1,9 @@
-def find_max(numbers): return max(numbers)
-def find_min(numbers): return min(numbers)
-def average(numbers): return sum(numbers)/len(numbers)
+def lcm(a, b): 
+    from math import gcd; 
+    return abs(a*b) // gcd(a, b)
+print('FIND LCM')
+a=int(input("enter Element 1:"))
+b=int(input("Enter Element 2:"))
+print(lcm(a,b))
+
+
